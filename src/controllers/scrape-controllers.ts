345e3740aa1f80425124c5a9ae94c3a9359c4485
Hercules <iraklistheofanidis@gmail.com>
2025-07-16 23:19:36 +0300

    scrape categories

--- src/controllers/scrape-controllers.ts
+++ src/controllers/scrape-controllers.ts
@@ -1,5 +1,7 @@
 import { ApiResponse } from "../db/models/ApiResponse";
 import { Request, Response } from "express";
+import puppeteer, { Browser, HTTPResponse } from 'puppeteer';
+import { closeBrowser, initializeBrowser } from "../db/helpers/puppeteer.handler";
 
 export async function scrapeBooks(req: Request, res: Response): Promise<ApiResponse<string[]>> {
     return {
@@ -22,9 +24,30 @@ export async function scrapeBookCategory(req: Request, res: Response): Promise<A
     }
 }
 
-export async function scrapeBookCategories(req: Request, res: Response): Promise<ApiResponse<string>> {
-    return {
-        statusCode: 200,
-        response: ['Scraped category 1', 'Scraped category 2', 'Scraped category 3'],
+export async function scrapeBookCategories(req: Request, res: Response): Promise<ApiResponse<{ title: string, urlToScrape: string }[]>> {
+    let browser: Browser | undefined = undefined;
+    try {
+        const page = await initializeBrowser('https://books.toscrape.com/');
+        if (!page) throw new Error('Failed to load page');
+        const categories = await page.$$eval('.nav.nav-list > li > ul > li > a', links =>
+            links.filter(link => link.textContent && link.getAttribute('href')).map(link => ({
+                title: link.textContent!.trim(),
+                urlToScrape: new URL(link.getAttribute('href')!, window.location.origin).href,
+            }))
+        );
+
+        return {
+            statusCode: 200,
+            response: categories,
+        }
+
+    } catch (error: any) {
+        return {
+            statusCode: 400,
+            error: error.message,
+        }
+
+    } finally {
+        closeBrowser(browser);
     }
 }
