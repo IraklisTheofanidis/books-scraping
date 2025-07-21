ab75f82890da45a24bdd9998be3b6342f4327111
Hercules <56442488+IraklisTheofanidis@users.noreply.github.com>
2025-07-22 01:00:35 +0300

    remove initialized route

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,6 @@ app.use(express.json({ limit: "75mb" }));
 
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World2!');
-});
-
 app.listen(port, () => {
   routes(app);
   console.log(`Server running at http://localhost:${port}`);
